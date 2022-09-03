e868e710c0ace4079cfe05d6832adad8be4b7eaa
Goldstein <root@goldstein.rs>
2022-09-03 20:31:18 +0300

    unbreak gpg

--- parts/nix.nix
+++ parts/nix.nix
@@ -22,6 +22,16 @@
     overlays = [
       inputs.neovim-nightly-overlay.overlay
       inputs.rust-overlay.overlays.default
+      (final: prev: {
+        gnupg = prev.gnupg.overrideAttrs (old: if old.version == "2.3.7" then {
+          patches = old.patches ++ [(final.fetchpatch {
+            name = "fix-yubikey.patch";
+            url = "https://gist.githubusercontent.com/GoldsteinE/529ebe029af14d43b4cf4cbb521b9869/raw/6b9415160a40dd4c6517831416e29c5d70971743/gpg.patch";
+            sha256 = "sha256-KD8H6N9oGx+85SetHx1OJku28J2dmsj/JkJudknCueU=";
+          })];
+        }
+        else {});
+      })
     ];
   };
 }
