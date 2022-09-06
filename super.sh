cf306784ae96c6636e76cc0387fe1a5f3879fad3
Goldstein <root@goldstein.rs>
2022-09-06 16:28:12 +0300

    fix super.sh

--- super.sh
+++ super.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 cleanup() {
-	doas chown -R root:root /etc/nixos
+	sudo chown -R root:root /etc/nixos
 }
 
 trap cleanup INT TERM
-doas chown -R goldstein /etc/nixos
+sudo chown -R goldstein /etc/nixos
 zsh
 cleanup
