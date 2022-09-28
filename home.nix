12e3a46d0e6b431a81244548809bcec729a8b93b
Goldstein <root@goldstein.rs>
2022-09-28 11:33:31 +0300

    use mold

--- home.nix
+++ home.nix
@@ -25,6 +25,14 @@ in
             | ${pkgs.findutils}/bin/xargs ${pkgs.xdotool}/bin/xdotool type
         '';
       };
+      ".cargo/config.toml".text = ''
+        [target.x86_64-unknown-linux-gnu]
+        linker = "clang"
+        rustflags = ["-C", "link-arg=-fuse-ld=${pkgs.mold}/bin/mold"]
+
+        [build]
+        rustc-wrapper = "${pkgs.sccache}/bin/sccache"
+      '';
     }) // pkgs.lib.genAttrs
       ([
         ".ghc"
