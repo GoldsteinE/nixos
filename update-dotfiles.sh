cfce39debf949af5d4875a1398a141d9fc07850e
Goldstein <root@goldstein.rs>
2022-10-21 21:09:01 +0300

    move conf to ~/sysconf

--- update-dotfiles.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-cd hosts/think
-nix flake lock --update-input dotfiles
-cd ../metal
-nix flake lock --update-input dotfiles
-cd ../..
-git add hosts
-git commit -m 'update dotfiles'
