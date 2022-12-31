c58d2a152378f0545a45f7065c79f7902bce4c57
Goldstein <root@goldstein.rs>
2022-12-31 17:45:06 +0300

    remove obsolete hack

--- configuration/parts/nix.nix
+++ configuration/parts/nix.nix
@@ -30,19 +30,6 @@
     overlays = [
       inputs.rust-overlay.overlays.default
       inputs.neovim-nightly-overlay.overlay
-      (final: prev: {
-        python3 = prev.python3.override {
-          packageOverrides = self: super: {
-            # https://github.com/NixOS/nixpkgs/issues/197408
-            dbus-next = super.dbus-next.overridePythonAttrs (old: {
-              checkPhase = builtins.replaceStrings
-                [ "not test_peer_interface" ]
-                [ "not test_peer_interface and not test_tcp_connection_with_forwarding" ]
-                old.checkPhase;
-            });
-          };
-        };
-      })
     ];
   };
 }
