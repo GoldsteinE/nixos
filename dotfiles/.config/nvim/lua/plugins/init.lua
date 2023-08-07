a7f3f7877b9ae3ef0be5a987d236cddb9c10afee
Goldstein <root@goldstein.rs>
2023-08-08 00:10:17 +0300

    better highlight

--- dotfiles/.config/nvim/lua/plugins/init.lua
+++ dotfiles/.config/nvim/lua/plugins/init.lua
@@ -11,6 +11,7 @@ return {
         'EdenEast/nightfox.nvim',
         config = function()
             vim.cmd [[ colorscheme nightfox ]]
+            vim.cmd [[ hi @text.literal gui=NONE ]]
         end,
     },
     {
