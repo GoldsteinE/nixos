9d482efa8ffdb2a22182c4406880d23e171a9834
Goldstein <root@goldstein.rs>
2024-08-21 15:45:44 +0000

    enable IPv6 on metal

--- modules/metal/networking.nix
+++ modules/metal/networking.nix
@@ -5,5 +5,16 @@
     firewall = {
       allowedTCPPorts = [ 7643 80 443 19423 ];
     };
+    interfaces.enp6s0.ipv6 = {
+      addresses = [{
+        address = "2a01:4f8:13b:f58::1";
+        prefixLength = 64;
+      }];
+      routes = [{
+        via = "fe80::1";
+        address = "::";
+        prefixLength = 0;
+      }];
+    };
   };
 }
