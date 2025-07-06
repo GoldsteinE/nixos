3d7353065c3f42b6442f7df9ab443fcb5381f2ce
Max Siling <root@goldstein.lol>
2025-07-06 17:10:09 +0300

    update

--- flake.nix
+++ flake.nix
@@ -14,12 +14,14 @@
     };
     # actual inputs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    lix = {
+      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
+      flake = false;
+    };
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.1.tar.gz";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
-      };
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.lix.follows = "lix";
     };
     naersk = {
       url = "github:nmattia/naersk";
